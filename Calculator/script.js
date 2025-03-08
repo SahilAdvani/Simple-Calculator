06a3e287bb36cb4d7e3552ee3fb0155c091cc263
Sahil Advani <sahil_2312res556@iitp.ac.in>
2025-03-08 11:48:28 +0530

    Recovered lost files and added them back

---
+++ Calculator/script.js
@@ -0,0 +1,7 @@
+let a = prompt("Enter first number:")
+let b = prompt("Enter second number:")
+
+alert(`Addition: \n${a} + ${b} = ${eval(`${a} + ${b}`)}`)
+alert(`Subtraction: \n${a} - ${b} = ${eval(`${a} - ${b}`)}`)
+alert(`Multiplication: \n${a} x ${b} = ${eval(`${a} * ${b}`)}`)
+alert(`Division: \n${a} / ${b} = ${eval(`${a} / ${b}`)}`)
